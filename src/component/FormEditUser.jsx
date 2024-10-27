ed127c63e19b9f2a7e41c500a5886db10c0673b2
joyboyid <ican.notfound@gmail.com>
2024-10-27 10:18:45 +0700

    bera-release

--- src/component/FormEditUser.jsx
+++ src/component/FormEditUser.jsx
@@ -16,7 +16,7 @@ const FormEditUser = () => {
   useEffect(() => {
     const getUserById = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/users/${id}`);
+        const response = await axios.get(`http://localhost:8800/users/${id}`);
         setName(response.data.name);
         setEmail(response.data.email);
         setTelpon(response.data.telp);
@@ -33,7 +33,7 @@ const FormEditUser = () => {
   const updateUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.patch(`http://localhost:5000/users/${id}`, {
+      await axios.patch(`http://localhost:8800/users/${id}`, {
         name: name,
         email: email,
         password: password,
